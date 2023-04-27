05dc57d4af4e3b44e560dbb4b5aa2b5f42dde1f5
Rishi Raj Jain <rjain@llnw.com>
2023-04-28 00:01:14 +0530

    use ref to load only if it's visible in the browser

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { Prefetch } from '@edgio/react'
+import useOnScreen from './useOnScreen'
+import { useEffect, useRef } from 'react'
 import { useRouter } from 'next/navigation'
+import { prefetch } from '@edgio/prefetch/window'
 
 const fallbackImage =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAH0AAACmCAMAAADAp3D7AAAAA1BMVEWAgICQdD0xAAAAK0lEQVR4nO3BMQEAAADCoPVP7W8GoAAAAAAAAAAAAAAAAAAAAAAAAAAAeANRtAABpXaWUQAAAABJRU5ErkJggg=='
@@ -30,33 +32,40 @@ const Item = (itemProps: ItemProps) => {
     : itemProps?.poster_path
     ? `https://opt.moovweb.net?quality=30&img=https://image.tmdb.org/t/p/original${itemProps?.poster_path}`
     : fallbackImage
+  const ref = useRef<HTMLAnchorElement>(null)
+  const isVisible = useOnScreen(ref)
+  useEffect(() => {
+    if (isVisible) {
+      // router.prefetch(`/show/${id}`)
+      // wanna cache this in the browser as well
+      prefetch(`/l0-themoviedb-api/movie/${id}`, 'fetch', { includeCacheMisses: true })
+      prefetch(`/l0-themoviedb-api/movie/${id}/credits`, 'fetch', { includeCacheMisses: true })
+    }
+  }, [isVisible])
   return (
-    <Prefetch url={`/l0-themoviedb-api/movie/${id}`}>
-      <a
-        href={`/show/${id}`}
-        onClick={(e) => {
-          e.preventDefault()
-          router.push(`/show/${id}`)
-        }}
-        className="flex flex-col min-w-[150px]"
-      >
-        <img
-          alt={name}
-          src={image}
-          width="150px"
-          height="225px"
-          loading="lazy"
-          className={['object-cover object-center min-h-[225px] rounded', !image && 'animate-pulse'].filter((i) => i).join(' ')}
-        />
-        <Prefetch url={`/l0-themoviedb-api/movie/${id}/credits`}>
-          {name ? (
-            <h3 className="mt-3 max-w-[150px] text-gray-300">{name}</h3>
-          ) : (
-            <div className="mt-3 w-[150px] h-[20px] bg-white/25 animate-pulse"></div>
-          )}
-        </Prefetch>
-      </a>
-    </Prefetch>
+    <a
+      ref={ref}
+      href={`/show/${id}`}
+      onClick={(e) => {
+        e.preventDefault()
+        router.push(`/show/${id}`)
+      }}
+      className="flex flex-col min-w-[150px]"
+    >
+      <img
+        alt={name}
+        src={image}
+        width="150px"
+        height="225px"
+        loading="lazy"
+        className={['object-cover object-center min-h-[225px] rounded', image === fallbackImage && 'animate-pulse'].filter((i) => i).join(' ')}
+      />
+      {name ? (
+        <h3 className="mt-3 max-w-[150px] text-gray-300">{name}</h3>
+      ) : (
+        <div className="mt-3 w-[150px] h-[20px] bg-white/25 animate-pulse"></div>
+      )}
+    </a>
   )
 }
 
