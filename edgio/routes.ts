e4aa1da1f56892c0aaa7405a1725f589e6e956d8
Rishi Raj Jain <rjain@llnw.com>
2023-04-27 09:49:06 +0530

    move routes

--- edgio/routes.ts
+++ edgio/routes.ts
@@ -10,21 +10,6 @@ dotenv.config({
 
 const router = new Router()
 
-if (isProductionBuild()) {
-  try {
-    const publicDir = readFileSync(join(process.cwd(), 'public_routes'), 'utf8')
-    publicDir.split(',').forEach((i) => {
-      router.match(`/${i}`, ({ serveStatic }) => {
-        serveStatic(`public/${i}`)
-      })
-    })
-  } catch (e) {}
-
-  router.match('/_next/static/:path*', ({ serveStatic }) => {
-    serveStatic('.next/static/:path*')
-  })
-}
-
 router.match('/service-worker.js', ({ serviceWorker }) => {
   serviceWorker('.edgio/temp/service-worker.js')
 })
@@ -40,18 +25,6 @@ router.get('/l0-api/:path*', ({ proxy, cache, removeUpstreamResponseHeader }) =>
     browser: false,
   })
   proxy('api', { path: '/:path*' })
-})
-
-router.get('/l0-themoviedb-image/:path*', ({ removeUpstreamResponseHeader, cache, proxy }) => {
-  removeUpstreamResponseHeader('set-cookie')
-  removeUpstreamResponseHeader('cache-control')
-  cache({
-    edge: {
-      maxAgeSeconds: 60 * 60 * 24 * 365,
-    },
-    browser: false,
-  })
-  proxy('image_themoviedb', { path: '/t/p/original/:path*' })
 })
 
 router.get('/l0-themoviedb-api/:path*', ({ proxy, cache, removeUpstreamResponseHeader }) => {
@@ -87,6 +60,19 @@ router.get('/_next/image', ({ cache, renderWithApp, removeUpstreamResponseHeader
 })
 
 if (isProductionBuild()) {
+  try {
+    const publicDir = readFileSync(join(process.cwd(), 'public_routes'), 'utf8')
+    publicDir.split(',').forEach((i) => {
+      router.match(`/${i}`, ({ serveStatic }) => {
+        serveStatic(`public/${i}`)
+      })
+    })
+  } catch (e) {}
+
+  router.match('/_next/static/:path*', ({ serveStatic }) => {
+    serveStatic('.next/static/:path*')
+  })
+
   const dynamicPaths = ['/', '/show/:id', '/play/:id']
 
   dynamicPaths.forEach((i) => {
